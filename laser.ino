ec0fa2a27e969088a304ef9fef4ba6959e1e3e31
Pradip Poudel <50986372+pradippoudel@users.noreply.github.com>
2020-04-24 09:23:38 +0545

    Add files via upload

---
+++ laser.ino
@@ -0,0 +1,16 @@
+//program to toggle on a led fofr 1 second
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(8,1);
+delay(100);
+digitalWrite(8,0);
+delay(100);
+
+}
